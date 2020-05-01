1346c3cf24a2f7dba9e200f99006d2d556d6fea8
Tristan Davis <tristan.seabrook@gmail.com>
2020-05-01 13:19:08 -0600

    added ping list module

---
+++ app/get_ping_list.js
@@ -0,0 +1,23 @@
+/*jshint esversion 8 */
+
+//return an array of IP addresses to ping based on config settings
+module.exports = (config) => {
+
+  switch(config.mode) {
+// if in directory mode, parse all csv, xls, and xlsx files in a given direcotry
+// for IP addresses
+    case 'directory':
+      return 'dir'
+      break;
+//if in filename mode, parse the specified file for IP addresses
+    case 'filename':
+      return 'file'
+      break;
+// if in range mode, expand the specified IP range into a list of IP addresses
+    case 'range':
+      return 'range'
+      break;
+    default:
+      return 'Invalid parameter specified for config.mode';
+  }
+};
--- index.js
+++ index.js
@@ -11,12 +11,14 @@ let yargs = require('yargs');
 //import modules
 let parseXLSX = require('./app/parse-xlsx');
 let args =      require('./app/args')();
+let getPingList = require('./app/get_ping_list');
 
-//set config parameters based on arguments received
-//set the script mode and any passed arguments
+//set the script mode and push parameters to the config object
 ((args) => {
   if (args.d) { config.mode = 'directory',  config.modeParams = args.d};
   if (args.f) { config.mode = 'filename',   config.modeParams = args.f};
   if (args.r) { config.mode = 'range',      config.modeParams = args.r};
   return 'directory';
 })(args);
+
+let pingList = getPingList(config);
