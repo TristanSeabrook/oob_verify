600a1830be65529b6cdf83273c815700f4675c4f
Tristan Davis <tristan.seabrook@gmail.com>
2020-05-18 15:15:36 -0600

    allow user to enter an octet as the second item in a range

--- app/parse_ip_range.js
+++ app/parse_ip_range.js
@@ -1,34 +1,56 @@
-/*jshint esversion: 8*/
-
+//import the ip module for IP address utilities
 let ip = require('ip');
+
+//regex string to match an octet in an IP address string
+let oct = '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)';
+
+//if the user provides only the last octet of the end IP address, return the
+//full IP based on the start IP address
+let getFullIP = (startIP, endIP) => {
+  let firstThreeOctsRegex = new RegExp(`^${oct}.${oct}.${oct}.`);
+  let lastOctRegex = new RegExp(`${oct}$`);
+  if (ip.isV4Format(endIP)) {
+    return endIP;
+  }
+  else {
+    let firstThreeStartOcts = startIP.match(firstThreeOctsRegex)[0];
+    let lastStartOct = startIP.match(lastOctRegex)[0];
+    let lastEndOct = endIP.match(lastOctRegex)[0];
+    if (parseInt(lastEndOct, 10) > parseInt(lastStartOct, 10)) {
+      return `${firstThreeStartOcts}${lastEndOct}`;
+    }
+  }
+};
+
+//given first and last address, generate an array of long notation
+//ip addresses
+    let getLongArr = (nextIP, endIP, ipArr = []) => {
+      let localIPArr = ipArr;
+      if (nextIP < endIP) {
+        localIPArr.push(nextIP);
+        getLongArr(nextIP + 1, endIP, localIPArr);
+      }
+      if (nextIP === endIP) {
+        localIPArr.push(endIP);
+      }
+      return localIPArr;
+    };
 
 module.exports = (config) => {
     let rangeString = config.modeParams;
-  //regex to parse octets in an IP string
-    let oct = '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)';
+  //regex to match a dotted decimal IP address in a string
     let ipRegex =     new RegExp(`${oct}.${oct}.${oct}.${oct}`, 'g');
-  //an array of IP addresses in a string and its first two values
+  //regex to match everything after a hypen and before the end of the line
+    let secondArgRegex =   new RegExp(`-.+$`);
+  //get the start and end IP addresses in the range
     let ipAddrs =     rangeString.match(ipRegex);
+    let secondArg =   rangeString.match(secondArgRegex)[0];
     let startIP =     ipAddrs[0];
-    let endIP =       ipAddrs[1];
+    let endIP =       getFullIP(startIP, secondArg);
+
   //convert from dotted decimal to long notation
     let longStartIP = parseInt(ip.toLong(startIP),  10);
     let longEndIP =  parseInt(ip.toLong(endIP),    10);
-
-  //given first and last address, generate an array of long notation
-  //ip addresses
-      let getLongArr = (nextIP, endIP, ipArr = []) => {
-        let localIPArr = ipArr;
-        if (nextIP < endIP) {
-          localIPArr.push(nextIP);
-          getLongArr(nextIP + 1, endIP, localIPArr);
-        }
-        if (nextIP === endIP) {
-          localIPArr.push(endIP);
-        }
-        return localIPArr;
-      };
-
     let longArr = getLongArr(longStartIP, longEndIP);
 
   //convert the array of long ip addresses to an array of dotted decimal
--- tests.js
+++ tests.js
@@ -26,6 +26,11 @@ let testRange = '100.100.129.202-100.100.129.232';
   let testRangeLast =   testRange.split('-')[1];
   let testRangeLength = 31;
 
+let testRange2 = '100.100.129.202-212';
+  let testRange2First =  testRange.split('-')[0];
+  let testRange2Last =   '100.100.129.212';
+  let testRange2Length = 11;
+
 let logNameRegex = /\d{6}_\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}.\w+$/g;
 let projNum = getProjNum(testFilename, {projRegex: /[1-9]\d{5}/});
 let getLogNameObj = {
@@ -42,6 +47,7 @@ let dirObj =    {
 let fileObj =   {mode: 'filename',  modeParams: testFile};
 let projNumObj = {projRegex: /[1-9]\d{5}/, altProjStr: 'PRIME'};
 let rangeObj =  {mode: 'range',     modeParams: testRange};
+let range2Obj = {mode: 'range',     modeParams: testRange2};
 let sheetObj = parseXlsx(testFile);
 
 //Tests
@@ -69,12 +75,15 @@ describe('parseIPRange', function() {
   });
   it('Should return the correct first value', function() {
     assert.equal(testRangeFirst, parseIPRange(rangeObj)[0]);
+    assert.equal(testRange2First, parseIPRange(range2Obj)[0]);
   });
   it('Should return the correct last value', function() {
     assert.equal(testRangeLast, parseIPRange(rangeObj)[testRangeLength - 1]);
+    assert.equal(testRange2Last, parseIPRange(range2Obj)[testRange2Length - 1]);
   });
   it('Should return an array of the correct length', function() {
     assert.equal(testRangeLength, parseIPRange(rangeObj).length);
+    assert.equal(testRange2Length, parseIPRange(range2Obj).length);
   });
 });
 
