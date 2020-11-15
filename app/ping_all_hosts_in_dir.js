6bc910d6e29658581b9a2352503329f07fd7fbc2
Tristan Davis <tristan.seabrook@gmail.com>
2020-11-15 12:33:01 -0700

    script exits if no files are found in directory mode

--- app/ping_all_hosts_in_dir.js
+++ app/ping_all_hosts_in_dir.js
@@ -7,15 +7,21 @@ let pingHostsInFile = require('../app/ping_hosts_in_file');
 
 module.exports = (config) => {
   const directory = config.modeParams;
-  const dirContentsArr = fs.readdirSync(directory);
+//create array of filenames in the specified directory, exluding hidden files
+  const dirContentsArr = fs.readdirSync(directory)
+    .filter(filename => ! filename.match(/^\./g));
 
-  for (const filename of dirContentsArr) {
-    let filePath = `${directory}/${filename}`;
-    console.log(`Parsing file \'${filePath}\'.`);
-    config.projNum = getProjNum(filename, config);
-    config.mode = filename;
-    config.modeParams = filePath;
-    pingHostsInFile(config);
+  if (! dirContentsArr[0]) {
+    console.log(`No parsable files found in ${config.modeParams}. Exiting.`);
   }
-
+  else {
+    for (const filename of dirContentsArr) {
+      let filePath = `${directory}/${filename}`;
+      console.log(`Parsing file \'${filePath}\'.`);
+      config.projNum = getProjNum(filename, config);
+      config.mode = filename;
+      config.modeParams = filePath;
+      pingHostsInFile(config);
+    }
+  }
 };
